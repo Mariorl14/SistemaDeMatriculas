8220f2ef3a968d851481e3c2880d00b579a12928
ABRAHAN MORALES <amorales30725@ufide.ac.cr>
2022-06-16 22:25:22 -0600

    Creacion de Create

--- FrontEnd/Controllers/HomeController.cs
+++ FrontEnd/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace FrontEnd.Controllers
             return View();
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
