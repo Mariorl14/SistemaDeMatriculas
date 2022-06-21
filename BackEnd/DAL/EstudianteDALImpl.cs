0c163872c3864b02408fe7b1724ba22652cfdba3
Angie <acubillo70234@ufide.ac.cr>
2022-06-20 21:41:30 -0600

    Creacion del Crud de Estudiantes

---
+++ BackEnd/DAL/EstudianteDALImpl.cs
@@ -0,0 +1,158 @@
+﻿using BackEnd.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.DAL
+{
+    public class EstudianteDALImpl : IEstudianteDAL
+    {
+        SISTEMA_ACADEMICOContext context;
+
+        public EstudianteDALImpl()
+        {
+            context = new SISTEMA_ACADEMICOContext();
+
+        }
+
+        public bool Add(Estudiante entity)
+        {
+            try
+            {
+                //sumar o calcular 
+
+                using (UnidadDeTrabajo<Estudiante> unidad = new UnidadDeTrabajo<Estudiante>(context))
+                {
+                    unidad.genericDAL.Add(entity);
+                    return unidad.Complete();
+                }
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public void AddRange(IEnumerable<Estudiante> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Estudiante> Find(Expression<Func<Estudiante, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Estudiante Get(int IdEstudiante)
+        {
+            try
+            {
+                Estudiante estudiante;
+                using (UnidadDeTrabajo<Estudiante> unidad = new UnidadDeTrabajo<Estudiante>(context))
+                {
+                    estudiante = unidad.genericDAL.Get(IdEstudiante);
+                }
+                return estudiante;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Estudiante> Get()
+        {
+            try
+            {
+                IEnumerable<Estudiante> estudiantes;
+                using (UnidadDeTrabajo<Estudiante> unidad = new UnidadDeTrabajo<Estudiante>(context))
+                {
+                    estudiantes = unidad.genericDAL.GetAll();
+                }
+                return estudiantes.ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public IEnumerable<Estudiante> GetAll()
+        {
+            try
+            {
+                IEnumerable<Estudiante> estudiantes;
+                using (UnidadDeTrabajo<Estudiante> unidad = new UnidadDeTrabajo<Estudiante>(context))
+                {
+                    estudiantes = unidad.genericDAL.GetAll();
+                }
+                return estudiantes;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Remove(Estudiante entity)
+        {
+            bool result = false;
+            try
+            {
+                using (UnidadDeTrabajo<Estudiante> unidad = new UnidadDeTrabajo<Estudiante>(context))
+                {
+                    unidad.genericDAL.Remove(entity);
+                    result = unidad.Complete();
+                }
+
+            }
+            catch (Exception)
+            {
+
+                result = false;
+            }
+
+            return result;
+        }
+
+        public void RemoveRange(IEnumerable<Estudiante> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Estudiante SingleOrDefault(Expression<Func<Estudiante, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Estudiante estudiante)
+        {
+            bool result = false;
+
+            try
+            {
+                using (UnidadDeTrabajo<Estudiante> unidad = new UnidadDeTrabajo<Estudiante>(context))
+                {
+                    unidad.genericDAL.Update(estudiante);
+                    result = unidad.Complete();
+                }
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ FrontEnd/Controllers/EstudianteController.cs
@@ -0,0 +1,133 @@
+﻿using BackEnd.DAL;
+using BackEnd.Entities;
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        IEstudianteDAL estudianteDAL;
+        IPlanDeEstudioDAL PlanDeEstudioDAL;
+
+        private EstudianteViewModel Convertir(Estudiante estudiante)
+        {
+            return new EstudianteViewModel
+            {
+                IdEstudiante = estudiante.IdEstudiante,
+                Nombre = estudiante.Nombre,
+                PrimerApellido = estudiante.PrimerApellido,
+                SegundoApellido = estudiante.SegundoApellido,
+                FechaNacimiento = estudiante.FechaNacimiento,
+                Genero = estudiante.Genero,
+                IdPlanEstudioFk = estudiante.IdPlanEstudioFk,
+                IdUsuarioFk = estudiante.IdUsuarioFk,
+                IdTipoUsuarioFk = estudiante.IdTipoUsuarioFk
+            };
+        }
+
+        #region Lista
+        public IActionResult Index()
+        {
+            List<Estudiante> lista;
+            estudianteDAL = new EstudianteDALImpl();
+            lista = estudianteDAL.GetAll().ToList();
+            List<EstudianteViewModel> estudiantes = new List<EstudianteViewModel>();
+
+            foreach (Estudiante estudiante in lista)
+            {
+                estudiantes.Add(Convertir(estudiante));
+            }
+
+
+            return View(estudiantes);
+        }
+
+        #endregion
+
+        #region Agregar
+        public IActionResult Create()
+        {
+            EstudianteViewModel estudiante = new EstudianteViewModel();
+            PlanDeEstudioDAL = new PlanDeEstudioDALImpl();
+
+            estudiante.Planes = PlanDeEstudioDAL.GetAll();
+
+            return View(estudiante);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Estudiante estudiante)
+        {
+            estudianteDAL = new EstudianteDALImpl();
+            estudianteDAL.Add(estudiante);
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        #region Detalles
+
+        public IActionResult Details(int id)
+        {
+            estudianteDAL = new EstudianteDALImpl();
+            EstudianteViewModel estudiante = Convertir(estudianteDAL.Get(id));
+
+            //UsuarioDAL = new UsuarioDALImpl();
+            //estudiante.Usuario = UsuarioDAL.Get((int)estudiante.IdUsuarioFk);
+
+            PlanDeEstudioDAL = new PlanDeEstudioDALImpl();
+            estudiante.Plan = PlanDeEstudioDAL.Get((int)estudiante.IdPlanEstudioFk);
+
+            return View(estudiante);
+        }
+        #endregion
+
+
+        #region  Editar
+
+
+        public IActionResult Edit(int id)
+        {
+            estudianteDAL = new EstudianteDALImpl();
+            EstudianteViewModel estudiante = Convertir(estudianteDAL.Get(id));
+
+            PlanDeEstudioDAL = new PlanDeEstudioDALImpl();
+
+            estudiante.Planes = PlanDeEstudioDAL.GetAll();
+
+            return View(estudiante);
+        }
+        [HttpPost]
+        public IActionResult Edit(Estudiante estudiante)
+        {
+            estudianteDAL = new EstudianteDALImpl();
+
+            estudianteDAL.Update(estudiante);
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+
+        #region Eliminar
+        public IActionResult Delete(int id)
+        {
+            estudianteDAL = new EstudianteDALImpl();
+            EstudianteViewModel estudiante = Convertir(estudianteDAL.Get(id));
+            return View(estudiante);
+        }
+        [HttpPost]
+        public IActionResult Delete(Estudiante estudiante)
+        {
+            estudianteDAL = new EstudianteDALImpl();
+
+            estudianteDAL.Remove(estudiante);
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+    }
+}
