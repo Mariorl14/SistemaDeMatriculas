4d884e622ec7ed80ef4cf47398fb4b2365d36be9
wagner201190 <90158672+wagner201190@users.noreply.github.com>
2022-07-26 19:25:31 -0600

    Reporte_1

--- MatriculasAPI/Program.cs
+++ MatriculasAPI/Program.cs
@@ -16,6 +16,24 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+app.MapPost("/reporte", () =>
+{
+    //Logica que obtenga los datos de la BD
+    //Convertir información a HTML
+    var htmlCode = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "Template\\invoice1.html");
+    SelectPdf.HtmlToPdf converter = new SelectPdf.HtmlToPdf();
+    SelectPdf.PdfDocument doc = converter.ConvertHtmlString(htmlCode);
+    //doc.Save(AppDomain.CurrentDomain.BaseDirectory + "Template\\invoice1.pdf");
+    byte[] data = doc.Save();
+    var result = Convert.ToBase64String(data);
+    doc.Close();
+    //{status = "ok", data = "result"};
+    return result;
+});
+
+
+
 app.UseStaticFiles();
 
 app.UseRouting();
