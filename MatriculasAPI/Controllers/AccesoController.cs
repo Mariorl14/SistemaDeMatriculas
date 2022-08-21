7c2393fb66a4d2512c4978deeb2764198e8e71cd
ABRAHAN MORALES <amorales30725@ufide.ac.cr>
2022-08-21 10:44:19 -0600

    Implementación Login

---
+++ MatriculasAPI/Controllers/AccesoController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MatriculasAPI.Models;
+using BackEnd.DAL;
+using BackEnd.Entities;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.Data.SqlClient;
+
+namespace MatriculasAPI.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(Usuario usuario)
+        {
+            UsuarioDALImpl usuarioDAL = new UsuarioDALImpl();
+
+            var usuarios = usuarioDAL.ValidarUsuario(usuario.Email, usuario.Contrasena);
+
+            if (usuarios != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim("Correo", usuarios.Email),
+                    new Claim(ClaimTypes.Role, usuarios.Rol)
+
+                };
+
+
+                var clamsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(clamsIdentity));
+
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+
+
+        }
+
+        public async Task<IActionResult> Salir()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Acceso");
+        }
+
+
+
+    }
+}
--- MatriculasAPI/Program.cs
+++ MatriculasAPI/Program.cs
@@ -1,10 +1,24 @@
+using BackEnd.Entities;
+using BackEnd.Utilities;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<SISTEMA_ACADEMICOContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+string connString = builder.Configuration.GetConnectionString("DefaultConnection");
+Util.ConnectionString = connString;
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(option =>
+    {
+        option.LoginPath = "/Home/Index";
+        option.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+        option.AccessDeniedPath = "/Home/Privacy";
+    });
 
 
 var app = builder.Build();
@@ -17,16 +31,16 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 
-
-
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Acceso}/{action=Index}/{id?}");
 
 app.Run();
